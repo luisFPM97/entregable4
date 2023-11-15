f2cabd04468e3cd4cfc67cb618a810394ca192b4
luisFPM97 <134348310+luisFPM97@users.noreply.github.com>
2023-11-15 10:02:10 -0500

    Update Alert.jsx

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -10,7 +10,7 @@ const Alert = ({isAlertDisable,setIsAlertDisable, isEdit,isAlertDelete}) => {
     <div className={`alert-container ${isAlertDisable && 'alert__disable'}`}>
         <section className='alert-containerinfo'>
         <h3 className='litle-info'>{`${isEdit && 'Actualizar' || isAlertDelete && 'Eliminar' || 'Crear'} usuario`}</h3>
-        <span className='alert-info'>{`'El usuario se ha '${isEdit && 'actualizado' || isAlertDelete && 'eliminado' || 'creado'}`}</span>
+        <span className='alert-info'>{`El usuario se ha ${isEdit && 'actualizado correctamente' || isAlertDelete && 'eliminado correctamente' || 'creado correctamente'}`}</span>
         <button className='btn_app' onClick={handleAcept}>Aceptar</button>
         </section>
     </div>
